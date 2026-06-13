6d8f13f9facb2174a4a49ad092d16828a3a1bebe
Ashish Kumar <90233808+DeveloperAshish8@users.noreply.github.com>
2026-06-13 08:47:31 +0530

    Add thumbnail extraction and update HTML structure

--- scripts/update-medium-posts.js
+++ scripts/update-medium-posts.js
@@ -3,21 +3,43 @@ const Parser = require("rss-parser");
 
 const parser = new Parser();
 
+function getThumbnail(post) {
+  const match = post.content?.match(/<img[^>]+src="([^"]+)"/);
+  return match ? match[1] : "";
+}
+
 async function main() {
   const feed = await parser.parseURL(
     "https://medium.com/feed/@ashish-4"
   );
 
-  const posts = feed.items
-    .slice(0, 5)
-    .map((post) => `- [${post.title}](${post.link})`)
+  const posts = feed.items.slice(0, 3);
+
+  const cards = posts
+    .map(
+      (post) => `
+<td width="33%" align="center">
+  <a href="${post.link}">
+    <img src="${getThumbnail(post)}" width="250" />
+    <br /><br />
+    <b>${post.title}</b>
+  </a>
+</td>`
+    )
     .join("\n");
+
+  const html = `
+<table>
+<tr>
+${cards}
+</tr>
+</table>`;
 
   const readme = fs.readFileSync("README.md", "utf8");
 
   const updated = readme.replace(
-    /<!-- BLOG-POST-LIST:START -->([\s\S]*?)<!-- BLOG-POST-LIST:END -->/,
-    `<!-- BLOG-POST-LIST:START -->\n${posts}\n<!-- BLOG-POST-LIST:END -->`
+    /<!-- BLOG-POST-LIST:START -->([\\s\\S]*?)<!-- BLOG-POST-LIST:END -->/,
+    `<!-- BLOG-POST-LIST:START -->\n${html}\n<!-- BLOG-POST-LIST:END -->`
   );
 
   fs.writeFileSync("README.md", updated);
